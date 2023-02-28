59fead495ef3fa6e4b61934156700f9a434574ba
batista <ekyidag@gmail.com>
2023-02-28 15:43:30 -0300

    first laravel version

---
+++ laravel-version/routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+function isPrime(int $num): bool
+{
+    if ($num < 1) return false;
+
+    for ($i = 2; $i < $num - 1; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Route::get('/', function ($request) {
+
+    $input = $request->count;
+
+    $requestDateTime = \Carbon\Carbon::now();
+    $startTimestamp = $requestDateTime->timestamp;
+
+    $elapsedTime = null;
+
+    $primes = [];
+    $n = 0;
+
+    while (count($primes) < $input) {
+        if (isPrime($n)) {
+            $primes[] = $n;
+        }
+
+        $n++;
+    }
+
+    $elapsedTime = Carbon::now()->diffInMilliseconds();
+
+    return response()->json([
+        'input' => $input,
+        'elapsedTime' => $elapsedTime,
+        'primes' => $primes
+    ]);
+});
--- readme.md
+++ readme.md
@@ -9,6 +9,11 @@
     * docker build . -t ekyidag/primes-app-php:$COMMIT_TAG -f ./php-version/Dockerfile
     * docker push ekyidag/primes-app-php:$COMMIT_TAG
       * deploy in k8s: kubectl apply -f deploy/primes-app-php.yaml
+  * laravel version
+    * COMMIT_TAG=$(git rev-parse --short HEAD); 
+    * docker build . -t ekyidag/primes-app-laravel:$COMMIT_TAG -f ./laravel-version/Dockerfile
+    * docker push ekyidag/primes-app-laravel:$COMMIT_TAG
+      * deploy in k8s: kubectl apply -f deploy/primes-app-laravel.yaml
 * Comandos uteis
   * (restart a deployment) kubectl rollout restart deployments/{name}
   * (get all logs from deployment) kubectl logs -f deployment/{name}
